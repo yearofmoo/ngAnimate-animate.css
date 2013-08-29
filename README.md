76af428f5752b873887cb883b3d3b4c3ae8cda78
Matias Niemelä <matias@yearofmoo.com>
2013-08-29 10:14:46 -0400

    added the rest of the animations

--- README.md
+++ README.md
@@ -19,19 +19,35 @@ JavaScript animation API.
 Once installed, animations can be performed by attaching the following CSS classes to an element.
 
 ### Flip Animations
-- **.flip-x** (runs flipInX for enter and show, runs flipOutX for leave and hide)
-- **.flip-y** (runs flipInY for enter and show, runs flipOutY for leave and hide)
+- **.dn-flip-x** (runs flipInX for enter, move and show, runs flipOutX for leave and hide)
+- **.dn-flip-y** (runs flipInY for enter, move and show, runs flipOutY for leave and hide)
 
 ### Fade Animations
-- **.fade** (runs fadeIn for enter and show, runs fadeOut for leave and hide)
-- **.fade-up** (runs fadeInUp for enter and show, runs fadeOutUp for leave and hide)
-- **.fade-down** (runs fadeInDown for enter and show, runs fadeOutDown for leave and hide)
-- **.fade-left** (runs fadeInLeft for enter and show, runs fadeOutLeft for leave and hide)
-- **.fade-right** (runs fadeInRight for enter and show, runs fadeOutRight for leave and hide)
+- **.dn-fade** (runs fadeIn for enter, move and show, runs fadeOut for leave and hide)
+- **.dn-fade-up** (runs fadeInUp for enter, move and show, runs fadeOutUp for leave and hide)
+- **.dn-fade-down** (runs fadeInDown for enter, move and show, runs fadeOutDown for leave and hide)
+- **.dn-fade-left** (runs fadeInLeft for enter, move and show, runs fadeOutLeft for leave and hide)
+- **.dn-fade-right** (runs fadeInRight for enter, move and show, runs fadeOutRight for leave and hide)
+- **.dn-fade-up-big** (runs fadeInUpBig for enter, move and show, runs fadeOutUpBig for leave and hide)
+- **.dn-fade-down-big** (runs fadeInDownBig for enter, move and show, runs fadeOutDownBig for leave and hide)
+- **.dn-fade-left-big** (runs fadeInLeftBig for enter, move and show, runs fadeOutLeftBig for leave and hide)
+- **.dn-fade-right-big** (runs fadeInRightBig for enter, move and show, runs fadeOutRightBig for leave and hide)
 
 ### Bounce Animations
-- **.bounce** (runs bounceIn for enter and show, runs bounceOut for leave and hide)
-- **.bounce-up** (runs bounceInUp for enter and show, runs bounceOutUp for leave and hide)
-- **.bounce-down** (runs bounceInDown for enter and show, runs bounceOutDown for leave and hide)
-- **.bounce-left** (runs bounceInLeft for enter and show, runs bounceOutLeft for leave and hide)
-- **.bounce-right** (runs bounceInRight for enter and show, runs bounceOutRight for leave and hide)
+- **.dn-bounce** (runs bounceIn for enter, move and show, runs bounceOut for leave and hide)
+- **.dn-bounce-up** (runs bounceInUp for enter, move and show, runs bounceOutUp for leave and hide)
+- **.dn-bounce-down** (runs bounceInDown for enter, move and show, runs bounceOutDown for leave and hide)
+- **.dn-bounce-left** (runs bounceInLeft for enter, move and show, runs bounceOutLeft for leave and hide)
+- **.dn-bounce-right** (runs bounceInRight for enter, move and show, runs bounceOutRight for leave and hide)
+
+### Bounce Animations
+- **.dn-rotate** (runs rotateIn for enter, move and show, runs rotateOut for leave and hide)
+- **.dn-rotate-down-left** (runs rotateInDownLeft for enter, move and show, runs rotateOutDownLeft for leave and hide)
+- **.dn-rotate-down-right** (runs rotateInDownRight for enter, move and show, runs rotateOutDownRight for leave and hide)
+- **.dn-rotate-up-left** (runs rotateInUpLeft for enter, move and show, runs rotateOutUpLeft for leave and hide)
+- **.dn-rotate-up-right** (runs rotateInUpRight for enter, move and show, runs rotateOutUpRight for leave and hide)
+
+### Other Animations
+- **.dn-lightspeed** (runs lightSpeedIn for enter, move and show, runs lightSpeedOut for leave and hide)
+- **.dn-roll** (runs rollIn for enter, move and show, runs rollOut for leave and hide)
+- **.dn-hinge** (runs fadeIn for enter, move and show, runs hinge for leave and hide)
--- animate.js
+++ animate.js
@@ -7,6 +7,7 @@ angular.module('ngAnimate-animate.css', ['ngAnimate'])
         var b = arguments[2];
         classNames = {
           enter : a,
+          move : a,
           leave : b,
           show : a,
           hide : b,
@@ -74,57 +75,111 @@ angular.module('ngAnimate-animate.css', ['ngAnimate'])
   //
   // Flip Animations
   //
-  .animation('.flip-x', ['animateCSSBuild', function(animateCSSBuild) {
-    return animateCSSBuild('flip-x','flipInX','flipOutX');
+  .animation('.dn-flip-x', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-flip-x','flipInX','flipOutX');
   }])
 
-  .animation('.flip-y', ['animateCSSBuild', function(animateCSSBuild) {
-    return animateCSSBuild('flip-y','flipInY','flipOutY');
+  .animation('.dn-flip-y', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-flip-y','flipInY','flipOutY');
   }])
 
   //
   // Fade Animations
   //
-  .animation('.fade', ['animateCSSBuild', function(animateCSSBuild) {
-    return animateCSSBuild('fade','fadeIn','fadeOut');
+  .animation('.dn-fade', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-fade','fadeIn','fadeOut');
   }])
 
-  .animation('.fade-up', ['animateCSSBuild', function(animateCSSBuild) {
-    return animateCSSBuild('fade-up','fadeInUp','fadeOutUp');
+  .animation('.dn-fade-up', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-fade-up','fadeInUp','fadeOutUp');
   }])
 
-  .animation('.fade-down', ['animateCSSBuild', function(animateCSSBuild) {
-    return animateCSSBuild('fade-down','fadeInDown','fadeOutDown');
+  .animation('.dn-fade-down', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-fade-down','fadeInDown','fadeOutDown');
   }])
 
-  .animation('.fade-left', ['animateCSSBuild', function(animateCSSBuild) {
-    return animateCSSBuild('fade-left','fadeInLeft','fadeOutLeft');
+  .animation('.dn-fade-left', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-fade-left','fadeInLeft','fadeOutLeft');
   }])
 
-  .animation('.fade-right', ['animateCSSBuild', function(animateCSSBuild) {
-    return animateCSSBuild('fade-right','fadeInRight','fadeOutRight');
+  .animation('.dn-fade-right', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-fade-right','fadeInRight','fadeOutRight');
+  }])
+
+  .animation('.dn-fade-up-big', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-fade-up-big','fadeInUpBig','fadeOutUpBig');
+  }])
+
+  .animation('.dn-fade-down-big', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-fade-down-big','fadeInDownBig','fadeOutDownBig');
+  }])
+
+  .animation('.dn-fade-left-big', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-fade-left-big','fadeInLeftBig','fadeOutLeftBig');
+  }])
+
+  .animation('.dn-fade-right-big', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-fade-right-big','fadeInRightBig','fadeOutRightBig');
   }])
 
 
   //
   // Bounce Animations
   //
-  .animation('.bounce', ['animateCSSBuild', function(animateCSSBuild) {
-    return animateCSSBuild('bounce','bounceIn','bounceOut');
+  .animation('.dn-bounce', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-bounce','bounceIn','bounceOut');
   }])
 
-  .animation('.bounce-up', ['animateCSSBuild', function(animateCSSBuild) {
-    return animateCSSBuild('bounce-up','bounceInUp','bounceOutUp');
+  .animation('.dn-bounce-up', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-bounce-up','bounceInUp','bounceOutUp');
   }])
 
-  .animation('.bounce-down', ['animateCSSBuild', function(animateCSSBuild) {
-    return animateCSSBuild('bounce-down','bounceInDown','bounceOutDown');
+  .animation('.dn-bounce-down', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-bounce-down','bounceInDown','bounceOutDown');
   }])
 
-  .animation('.bounce-left', ['animateCSSBuild', function(animateCSSBuild) {
-    return animateCSSBuild('bounce-left','bounceInLeft','bounceOutLeft');
+  .animation('.dn-bounce-left', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-bounce-left','bounceInLeft','bounceOutLeft');
   }])
 
-  .animation('.bounce-right', ['animateCSSBuild', function(animateCSSBuild) {
-    return animateCSSBuild('bounce-right','bounceInRight','bounceOutRight');
+  .animation('.dn-bounce-right', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-bounce-right','bounceInRight','bounceOutRight');
+  }])
+
+  //
+  // Rotate Animations
+  //
+  .animation('.dn-rotate', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-rotate','rotateIn','rotateOut');
+  }])
+
+  .animation('.dn-rotate-up-left', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-rotate-up-left','rotateInUpLeft','rotateOutUpLeft');
+  }])
+
+  .animation('.dn-rotate-down-left', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-rotate-down-left','rotateInDownLeft','rotateOutDownLeft');
+  }])
+
+  .animation('.dn-rotate-up-right', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-rotate-up-right','rotateInUpRight','rotateOutUpRight');
+  }])
+
+  .animation('.dn-rotate-down-right', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-rotate-down-right','rotateInDownRight','rotateOutDownRight');
+  }])
+
+  //
+  // Other Animations
+  //
+  .animation('.dn-lightspeed', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-lightspeed','lightSpeedIn','lightSpeedOut');
+  }])
+
+  .animation('.dn-roll', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-roll','rollIn','rollOut');
+  }])
+
+  .animation('.dn-hinge', ['animateCSSBuild', function(animateCSSBuild) {
+    return animateCSSBuild('dn-hinge','fadeIn','hinge');
   }])
