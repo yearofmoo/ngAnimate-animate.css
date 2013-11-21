340ce3647282386b6ac8af76f7b2f036dbb7b0c3
Tero Alén <tero@alen.mobi>
2013-11-21 14:38:02 +0200

    Add missing semicolon end of animate.js

--- animate.js
+++ animate.js
@@ -199,4 +199,4 @@ angular.module('ngAnimate-animate.css', ['ngAnimate'])
 
   .animation('.dn-hinge', ['animateCSSBuild', function(animateCSSBuild) {
     return animateCSSBuild('dn-hinge','fadeIn','hinge');
-  }])
+  }]);
